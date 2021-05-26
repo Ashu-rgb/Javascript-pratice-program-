32e6d3994f189aaca81b278f960c66e496e709f9
Ashutosh <ashutosh.jha@softobiz.com>
2021-05-26 11:27:27 +0530

    intialize file

---
+++ second.js
@@ -0,0 +1,15 @@
+//addition
+add = 5 + 2;
+document.write(add+"<br>");
+ 
+//subtraction
+sub = 5 - 2;
+document.write(sub+"<br>");
+ 
+//multiplication
+mul = 5 * 2;
+document.write(mul+"<br>");
+ 
+//division
+div = 5 / 2;
+document.write(div+"<br>");
